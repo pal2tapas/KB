50d7eb5956a5e83c9f08c3a65c02daa77529acc1
manojkumar <manoj@Manojs-MacBook-Pro.local>
2018-03-10 09:17:39 +0530

    First commit

---
+++ AirAsia-Interview/AirAsia-Interview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirAsia-Interview
+//
+//  Created by Manoj Gundluru on 10/03/18.
+//  Copyright © 2018 Manoj Gundluru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
